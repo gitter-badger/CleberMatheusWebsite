c481d2ab4407cad40e42076ff38f5f9a9e542df7
Cleber Matheus <clebermatheus@outlook.com>
2014-01-12 17:32:43 -0200

    Update 50 - 12/1/2014 - v0.9.8.2

--- videos/gokaiger/gokaiger.js
+++ videos/gokaiger/gokaiger.js
@@ -1,13 +1,23 @@
 ﻿var gokaiger = document.getElementById('src1');
 var govideo1 = document.getElementById('src2');
 var video1 = document.getElementById('video1');
-function ep1(){
-	//gokaiger.src="https://googledrive.com/host/0B5QjfaSWUJ0VeGhRTThONVpPZGM/ep1.mp4";
-	//govideo1.src="https://googledrive.com/host/0B5QjfaSWUJ0VeGhRTThONVpPZGM/ep1.ogv";
+function eps(){
 	video1.load();
 	document.getElementById("video1").style.display="block";
 	document.getElementById("player1").style.display="block";
 	document.getElementById("black_overlay").style.display="block";
+}
+function ep1(){
+	video1.poster = "ep1.jpg";
+	gokaiger.src="https://googledrive.com/host/0B5QjfaSWUJ0VeGhRTThONVpPZGM/ep1.mp4";
+	govideo1.src="https://googledrive.com/host/0B5QjfaSWUJ0VeGhRTThONVpPZGM/ep1.ogv";
+	eps();
+}
+function ep2(){
+	video1.poster = "http://static3.wikia.nocookie.net/__cb20110522012161/powerrangers/images/1/13/Shinkenboy.png";
+	gokaiger.src="ep2.mp4";
+	govideo1.src="ep2.webm";
+	eps();
 }
 function play2(){
 	document.getElementById('video1').style.display='none';
